4d41f6dfd21a87177d7ec964ee54cad3707a80bf
sararin <32796093+sararin@users.noreply.github.com>
2018-09-20 10:22:07 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject lorem-ipsum "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "outputs lorem-ipsumesque text"
+  :license {:name "BSD-3"
+            :url "https://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot lorem-ipsum.core
   :target-path "target/%s"
